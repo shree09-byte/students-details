356384a7d0dd25b3d1d88e612a055192a70bdd60
shree09-byte <shrijoshi34@gmail.com>
2024-11-08 14:58:26 +0530

    Source Code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { ReactiveFormsComponent } from './reactiveForm/reactive-forms/reactive-forms.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'students-details';
+export class AppComponent implements OnInit {
+  @ViewChild('myModal') modal: ElementRef | undefined;
+  studentObj: Student = new Student();
+  studentList: Student[] = [];
+
+  ngOnInit(): void {
+    const localData = localStorage.getItem('students-details');
+    if (localData != null) {
+      this.studentList = JSON.parse(localData);
+    }
+  }
+
+  openModal() {
+    const modal = document.getElementById('myModal');
+    if (modal !== null) {
+      modal.style.display = 'block';
+    }
+  }
+
+  editStudent(item: Student) {
+    this.studentObj = item;
+    this.openModal();
+  }
+
+  updateStudent() {
+    const currentRecord = this.studentList.find(
+      (x) => x.id == this.studentObj.id
+    );
+    if (currentRecord != undefined) {
+      currentRecord.name = this.studentObj.name;
+      currentRecord.mobile = this.studentObj.mobile;
+      currentRecord.city = this.studentObj.city;
+      currentRecord.state = this.studentObj.state;
+      currentRecord.pincode = this.studentObj.pincode;
+      currentRecord.address = this.studentObj.address;
+      currentRecord.email = this.studentObj.email;
+      localStorage.setItem(
+        'students-details',
+        JSON.stringify(this.studentList)
+      );
+      this.closeModal();
+    }
+  }
+
+  deleteStudent(item: Student) {
+    const isConfirm = confirm('Are you sure you want to delete this record?');
+    if (isConfirm) {
+      const currentRecord = this.studentList.findIndex((x) => x.id == item.id);
+      this.studentList.splice(currentRecord, 1);
+      localStorage.setItem(
+        'students-details',
+        JSON.stringify(this.studentList)
+      );
+    }
+  }
+
+  closeModal() {
+    this.studentObj = new Student();
+    if (this.modal != null) {
+      this.modal.nativeElement.style.display = 'none';
+    }
+  }
+
+  saveStudent() {
+    // debugger;
+    const isLocalPresent = localStorage.getItem('students-details');
+    if (isLocalPresent != null) {
+      const students = JSON.parse(isLocalPresent);
+      this.studentObj.id = students.length + 1;
+      students.push(this.studentObj);
+      this.studentList = students;
+      localStorage.setItem('students-details', JSON.stringify(students));
+    } else {
+      const newArr = [];
+      newArr.push(this.studentObj);
+      this.studentObj.id = 1;
+      this.studentList = newArr;
+      localStorage.setItem('students-details', JSON.stringify(newArr));
+    }
+    this.closeModal();
+  }
+}
+
+export class Student {
+  id: number = 0;
+  name: string = '';
+  mobile: string = '';
+  city: string = '';
+  state: string = '';
+  pincode: string = '';
+  address: string = '';
+  email: string = '';
+
+  constructor() {
+    this.id = 0;
+    this.name = '';
+    this.mobile = '';
+    this.city = '';
+    this.state = '';
+    this.pincode = '';
+    this.address = '';
+    this.email = '';
+  }
 }
---
+++ src/app/reactiveForm/reactive-forms/reactive-forms.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import {
+  FormGroup,
+  FormBuilder,
+  Validators,
+  ReactiveFormsModule,
+} from '@angular/forms';
+
+@Component({
+  selector: 'app-reactive-forms',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './reactive-forms.component.html',
+  styleUrl: './reactive-forms.component.scss',
+})
+export class ReactiveFormsComponent {
+  registerForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm.value);
+    } else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+}
